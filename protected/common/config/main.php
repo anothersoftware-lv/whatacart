11d260d1d2b527e8994f0ab795e1325ad637647e
lauris@weberp.lv <latpages@gmail.com>
2019-10-19 20:13:36 +0300

    Separated image folders via custom AssetManager
    (cherry picked from commit 94b6a4450e70cceed2b462e547cb6e3d153b0c15)

--- protected/common/config/main.php
+++ protected/common/config/main.php
@@ -2,6 +2,9 @@
 use yii\helpers\ArrayHelper;
 
 $imageManagerClass  = 'usni\library\web\ImageManager';
+$manufacturerImageManagerClass  = 'common\modules\manufacturer\web\ImageManager';
+$productImageManagerClass  = 'products\web\ImageManager';
+//$productCategoryImageManagerClass  = 'common\modules\asl_catalog\web\ImageManager'; //@FIXME - should be added dynamically in module (not config dependent)
 $fileManagerClass   = 'usni\library\web\FileManager';
 $videoManagerClass  = 'usni\library\web\VideoManager';
 return ArrayHelper::merge(
@@ -24,12 +27,21 @@ return ArrayHelper::merge(
                                                 'productWeightManager'      => ['class' => 'products\managers\ProductWeightManager'],
                                                 'productDimensionManager'   => ['class' => 'products\managers\ProductDimensionManager'],
                                                 'assetManager'      => [
+                                                                            'class' => 'common\web\AssetManager',
                                                                             'resourcesPath' => APPLICATION_PATH . '/resources',
                                                                             'fileUploadPath' => APPLICATION_PATH . '/resources/files',
                                                                             'imageUploadPath' => APPLICATION_PATH . '/resources/images',
                                                                             'thumbUploadPath' => APPLICATION_PATH . '/resources/images/thumbs',
+                                                                            'manufacturerImageUploadPath' => APPLICATION_PATH . '/resources/images/manufacturer',
+                                                                            'manufacturerThumbUploadPath' => APPLICATION_PATH . '/resources/images/manufacturer/thumbs',
+                                                                            'productImageUploadPath' => APPLICATION_PATH . '/resources/images/product',
+                                                                            'productThumbUploadPath' => APPLICATION_PATH . '/resources/images/product/thumbs',
+                                                                            'productCategoryImageUploadPath' => APPLICATION_PATH . '/resources/images/product_category',
+                                                                            'productCategoryThumbUploadPath' => APPLICATION_PATH . '/resources/images/product_category/thumbs',
                                                                             'videoUploadPath' => APPLICATION_PATH . '/resources/videos',
                                                                             'imageManagerClass'  => $imageManagerClass,
+                                                                            'productImageManagerClass'  => $productImageManagerClass,
+                                                                            //'productCategoryImageManagerClass'  => $productCategoryImageManagerClass,
                                                                             'fileManagerClass'   => $fileManagerClass,
                                                                             'videoManagerClass'  => $videoManagerClass
                                                                         ],
